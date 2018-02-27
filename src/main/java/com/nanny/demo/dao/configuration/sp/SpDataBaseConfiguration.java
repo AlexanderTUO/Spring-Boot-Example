c21e6150079187a00efcf70c797afc8b9b4d6922
zhongchaojun <zhongchaojun@mobike.com>
2018-02-27 17:33:58 +0800

    remove some code
    Signed-off-by: zhongchaojun <zhongchaojun@mobike.com>

--- src/main/java/com/nanny/demo/dao/configuration/sp/SpDataBaseConfiguration.java
+++ src/main/java/com/nanny/demo/dao/configuration/sp/SpDataBaseConfiguration.java
@@ -3,23 +3,15 @@ package com.nanny.demo.dao.configuration.sp;
 import com.nanny.demo.dao.configuration.DaoConfigurationBase;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ConfigurationProperties(prefix = "sp.datasource")
 @MapperScan(basePackages = "com.nanny.demo.dao.mapper.sp",
         sqlSessionFactoryRef = "spSession")
 public class SpDataBaseConfiguration extends DaoConfigurationBase {
-
-    @Bean("spDatabase")
-    public DataSource getDataSource() {
-        return super.dataSource();
-    }
 
     @Bean("spSession")
     public SqlSessionFactory getSqlSessionFactory() throws Exception {
--- src/main/java/com/nanny/demo/dao/configuration/sq/SqDataBaseConfiguration.java
+++ src/main/java/com/nanny/demo/dao/configuration/sq/SqDataBaseConfiguration.java
@@ -7,18 +7,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 @ConfigurationProperties(prefix = "sq.datasource")
 @MapperScan(basePackages = "com.nanny.demo.dao.mapper.sq",
         sqlSessionFactoryRef = "sqSession")
 public class SqDataBaseConfiguration extends DaoConfigurationBase {
-
-    @Bean("sqDatabase")
-    public DataSource getDataSource() {
-        return super.dataSource();
-    }
 
     @Bean("sqSession")
     public SqlSessionFactory getSqlSessionFactory() throws Exception {
