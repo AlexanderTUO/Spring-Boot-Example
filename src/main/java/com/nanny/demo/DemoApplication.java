7dd67552074b39deba9d67d1fd68eb9dcfd66935
zhongchaojun <zhongchaojun@mobike.com>
2018-02-27 20:08:41 +0800

    update project
    Signed-off-by: zhongchaojun <zhongchaojun@mobike.com>

--- src/main/java/com/nanny/demo/DemoApplication.java
+++ src/main/java/com/nanny/demo/DemoApplication.java
@@ -4,12 +4,10 @@ import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         MybatisAutoConfiguration.class})
-@EnableConfigurationProperties
 public class DemoApplication {
 
     public static void main(String[] args) {
