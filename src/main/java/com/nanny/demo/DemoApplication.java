eafb6e5a99bae3de52f16ed21b7e6471003b815d
zhongchaojun <zhongchaojun@mobike.com>
2018-02-27 14:40:19 +0800

    update project

--- src/main/java/com/nanny/demo/DemoApplication.java
+++ src/main/java/com/nanny/demo/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.nanny.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.nanny.demo.dao.mapper")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
--- src/main/java/com/nanny/demo/controller/DemoController.java
+++ src/main/java/com/nanny/demo/controller/DemoController.java
@@ -1,9 +1,23 @@
 package com.nanny.demo.controller;
 
+import com.nanny.demo.biz.DemoBiz;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("demo")
 public class DemoController {
+    private DemoBiz demoBiz;
+
+    @Autowired
+    DemoController(DemoBiz demoBiz) {
+        this.demoBiz = demoBiz;
+    }
+
+    @RequestMapping(value = "info", method = RequestMethod.GET)
+    public int getDemoInfo() {
+        return demoBiz.getCityByPrimaryKey(1).getCol2();
+    }
 }
